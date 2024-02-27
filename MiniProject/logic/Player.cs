1e2ac2936379f2efeed517aa2b0c85c75ad213c3
DingusSchool <1075003@hr.nl>
2024-02-27 14:08:29 +0100

    update Player.cs
    inventory en HP toegevoegd

--- MiniProject/logic/Player.cs
+++ MiniProject/logic/Player.cs
@@ -4,14 +4,14 @@ public class Player{
 
     public int PlayerLevel {get; set;}
     public int PlayerMapPosition {get; set;} //Location ID
-    public List<Item> ?inventory {get; set;}
+    public Inventory inventory = new Inventory();
+    public int HP { get; set; }
 
     public Player(){
 
         PlayerLevel = 1;
         PlayerMapPosition = 1;
-        inventory = new();
-        
+        HP = 90 * (PlayerLevel * 10);
     }
 
 }
