1e5e968a0f325ad6b8e183efe62f9fca163df8b5
DingusSchool <1075003@hr.nl>
2024-02-27 13:52:06 +0100

    added Inventory.cs en gitignore
    Inventory.cs aangemaakt en Inventory Field toegevoegd aan Player.cs

--- MiniProject/obj/Debug/net7.0/MiniProject.AssemblyInfo.cs
+++ MiniProject/obj/Debug/net7.0/MiniProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ef1381a6aa7c431ef516bc02ba96dd878267df")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
